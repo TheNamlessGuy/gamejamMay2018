8fc4b0b2ef455cc8b1ba9b05d476c58965ed113a
Maximilian Bragazzi Ihrén <bragux@hotmail.com>
2018-05-05 20:19:47 +0200

    crycrycry

--- MainMenu.py
+++ MainMenu.py
@@ -9,6 +9,7 @@ class MainMenu(WorldInterface):
         WorldInterface.__init__(self)
         self.bg = Sprite(load_image('res/main_menu.png'), Vec2(320, 240), (640, 480))
         self.space = Sprite(load_image('res/spacebar.png'), Vec2(320, 420), (310, 54))
+        self.tear = Sprite(load_image('res/tear.png'), Vec2(388, 247), (8, 8))
 
     def update(self, gs):
         if gs['keyboard']['ctrl-action']:
@@ -18,6 +19,15 @@ class MainMenu(WorldInterface):
         self.space.pos.x = 320 - math.cos(math.radians(pygame.time.get_ticks())) * 5
         self.space.pos.y = 420 + math.sin(math.radians(pygame.time.get_ticks())) * 5
 
+        self.tear.pos.y += (self.tear.pos.y / self.tear_offset)
+        self.tear_offset -= 4
+        if (self.tear.pos.y > 480):
+            self.tear.pos.y = 247
+            self.tear_offset = 100
+
     def reset(self, gs):
+        self.tear_offset = 100
+        
         self.sprites.append(self.bg)
+        self.sprites.append(self.tear)
         self.sprites.append(self.space)
